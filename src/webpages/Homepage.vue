847f51de6f0ccabfd97b41ccc4dd9450e5765b0e
Swathie <swathie.s@gmail.com>
2023-06-13 14:56:33 -0400

    Changed login page and purchase page

--- src/webpages/Homepage.vue
+++ src/webpages/Homepage.vue
@@ -2,24 +2,6 @@
 import Header from "../components/Header.vue";
 import Hero from "../components/Hero.vue";
 import Footer from "../components/Footer.vue";
-import axios from "axios";
-import { useStore } from "../store/index";
-
-const store = useStore();
-const movies = (
-  await axios.get("https://api.themoviedb.org/3/movie/popular", {
-    params: {
-      api_key: import.meta.env.VITE_TMDB_API_KEY,
-    },
-  })
-).data;
-
-store.movies = movies.results.map((movie) => {
-  return {
-    id: movie.id,
-    poster: movie.poster_path,
-  };
-});
 </script>
 
 <template>
--- src/webpages/LoginPage.vue
+++ src/webpages/LoginPage.vue
@@ -19,7 +19,7 @@ const passwordTwo = ref("");
 
 const registerViaEmail = async () => {
   if (passwordOne.value !== passwordTwo.value) {
-    alert("Brozzzzz..... Yer Paswerdz not coolz!");
+    alert("You're currently facing short-term memory loss; you typed the password incorrectly.");
     return;
   }
 
@@ -57,43 +57,51 @@ const registerViaGoogle = async () => {
 </script>
 
 <template>
-  <div class="auth-container">
-    <div>
-      <h1>Register via Google</h1>
-      <button @click="registerViaGoogle()">Google</button>
-    </div>
-    <div>
-      <h1>Register via Email</h1>
-      <form class="setup" @submit.prevent="registerViaEmail()">
-        <input v-model="email" type="email" placeholder="email" />
-        <input
-          v-model="passwordOne"
-          type="password"
-          placeholder="Enter Password"
-        />
-        <input
-          v-model="passwordTwo"
-          type="password"
-          placeholder="Re-enter Password"
-        />
-        <input type="submit" value="Register" />
-      </form>
-      <hr />
-      <h1>Login via Email</h1>
-      <form class="login" @submit.prevent="loginViaEmail()">
-        <input v-model="email" type="email" placeholder="Email" />
-        <input v-model="passwordOne" type="password" placeholder="Password" />
-        <input type="submit" value="Login" />
-      </form>
+  <div class="bg-image">
+    <div class="auth-container">
+      <div>
+        <h1>Register via Google</h1>
+        <button @click="registerViaGoogle()">Google</button>
+      </div>
+      <div>
+        <h1>Register via Email</h1>
+        <form class="setup" @submit.prevent="registerViaEmail()">
+          <input v-model="email" type="email" placeholder="email" />
+          <input
+            v-model="passwordOne"
+            type="password"
+            placeholder="Enter Password"
+          />
+          <input
+            v-model="passwordTwo"
+            type="password"
+            placeholder="Re-enter Password"
+          />
+          <input type="submit" value="Register" />
+        </form>
+        <hr />
+        <h1>Login via Email</h1>
+        <form class="login" @submit.prevent="loginViaEmail()">
+          <input v-model="email" type="email" placeholder="Email" />
+          <input v-model="passwordOne" type="password" placeholder="Password" />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.bg-image {
+  width: 100vw;
+  height: 100vh;
+  /* background-image: url(https://static.displate.com/857x1200/displate/2021-03-12/758b90b3aa7f6aa1d3f64b20778ed3ec_f85353a0748ebdc4176204f4c5342850.jpg); */
+  background-size: cover;
+}
 .auth-container {
-  background-image: url(https://static.displate.com/857x1200/displate/2021-03-12/758b90b3aa7f6aa1d3f64b20778ed3ec_f85353a0748ebdc4176204f4c5342850.jpg);
   display: flex;
   gap: 5rem;
+  background-color: rgb(178, 206, 255);
 }
 
 .setup,
@@ -103,80 +111,3 @@ const registerViaGoogle = async () => {
   gap: 1rem;
 }
 </style>
-
-<!-- <script setup>
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-const username = ref("");
-const password = ref("");
-
-const login = () => {
-  if (username.value === "tmdb" && password.value === "movies") {
-    router.push("/purchase");
-  } else {
-    alert("Invalid username/password!");
-  }
-};
-</script>
-
-<template>
-  <div id="background">
-    <form class="login-container" @submit.prevent="login()">
-      <h2>Sign in</h2>
-      <input type="text" placeholder="username" v-model="username" />
-      <input type="password" placeholder="password" v-model="password" />
-      <input type="submit" id="button" value="Login" />
-    </form>
-  </div>
-</template>
-
-<style>
-#background {
-  background-color: rgb(1, 1, 48);
-  width: 100vw;
-  height: 100vh;
-  left: 0;
-  top: 0;
-  position: fixed;
-}
-
-.login-container {
-  border-radius: 25px;
-  margin: auto;
-  padding: 50px;
-  width: 500px;
-  height: 550px;
-  align-items: center;
-  background-color: #ddddff46;
-  display: flex;
-  flex-direction: column;
-  margin-top: 250px;
-  color: white;
-  filter: drop-shadow(-10px 10px 10px #7c83a5);
-  gap: 15px;
-}
-
-input {
-  padding: 20px;
-  margin: 10px;
-  border-radius: 7px;
-}
-
-#button {
-  text-align: right;
-  border-radius: 12px;
-}
-
-#button:hover {
-  background-color: #cddcff;
-  color: rgb(1, 1, 48);
-  filter: drop-shadow(-10px 10px 10px #7c83a5);
-}
-
-h2 {
-  font-size: 45px;
-  padding: 30px;
-}
-</style> -->
