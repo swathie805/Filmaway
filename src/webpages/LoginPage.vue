a66398f19f4a6b13e65d69ac84df0ad7f6b9f665
swathie <swathie.s@gmail.com>
2023-06-15 00:13:00 -0400

    Styled login page

--- src/webpages/LoginPage.vue
+++ src/webpages/LoginPage.vue
@@ -62,13 +62,22 @@ const registerViaGoogle = async () => {
 </script>
 
 <template>
-  <div class="bg-image">
+   <img
+    id="bg-image"
+    :src="`https://media.istockphoto.com/id/1364376802/vector/glow-modern-background-blur.jpg?s=612x612&w=0&k=20&c=SBOz5Ge_Q_jidQ--q2Cud70BJk8AOcdnWFRT1jh65BQ=`"
+    alt=""
+  />
     <div class="auth-container">
-      <div>
-        <h1>Register via Google</h1>
-        <button @click="registerViaGoogle()">Google</button>
+      <div id="login">
+        <h1>Login via Email</h1>
+        <form class="login" @submit.prevent="loginViaEmail()">
+          <input v-model="email" type="email" placeholder="Email" />
+          <input v-model="passwordOne" type="password" placeholder="Password" />
+          <input class="btn" type="submit" value="Login" />
+        </form>
       </div>
-      <div>
+      <div class="vl"></div>
+      <div class="register">
         <h1>Register via Email</h1>
         <form class="setup" @submit.prevent="registerViaEmail()">
           <input v-model="email" type="email" placeholder="email" />
@@ -82,31 +91,43 @@ const registerViaGoogle = async () => {
             type="password"
             placeholder="Re-enter Password"
           />
-          <input type="submit" value="Register" />
+          <input class="btn" type="submit" value="Register" />
         </form>
-        <hr />
-        <h1>Login via Email</h1>
-        <form class="login" @submit.prevent="loginViaEmail()">
-          <input v-model="email" type="email" placeholder="Email" />
-          <input v-model="passwordOne" type="password" placeholder="Password" />
-          <input type="submit" value="Login" />
-        </form>
+        <!-- <button class="google-login" @click="registerViaGoogle()"> <img src="https://cdn-icons-png.flaticon.com/512/270/270014.png" /></button> -->
+        <img class="google-login" @click="registerViaGoogle()" src="https://cdn-icons-png.flaticon.com/512/270/270014.png" />
       </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-.bg-image {
+#bg-image {
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100vw;
   height: 100vh;
-  /* background-image: url(https://static.displate.com/857x1200/displate/2021-03-12/758b90b3aa7f6aa1d3f64b20778ed3ec_f85353a0748ebdc4176204f4c5342850.jpg); */
-  background-size: cover;
+  z-index: -1;
 }
 .auth-container {
+  border-radius: 10px;
+  margin: auto;
+  padding: 60px 30px;
+  font-family: sans-serif;
+  color: white;
+  background-color: rgba(177, 182, 255, 0.25);
+  filter: drop-shadow(-10px 10px 20px #827397);
+  margin-top: 200px;
+  height: 500px;
+  width: 40%;
   display: flex;
-  gap: 5rem;
-  background-color: rgb(178, 206, 255);
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.vl{
+  border-left: 6px solid white;
+  height: 400px;
 }
 
 .setup,
@@ -114,5 +135,34 @@ const registerViaGoogle = async () => {
   display: flex;
   flex-direction: column;
   gap: 1rem;
+}
+
+.register{
+  margin-right: 40px;
+}
+
+#login {
+  margin-left: 35px;
+  margin-bottom: 60px;
+}
+
+.google-login {
+  border: transparent;
+  width: 100px;
+  vertical-align: center;
+}
+
+.btn {
+  background-color: rgb(3, 22, 85) ;
+  color: white;
+  border: transparent;
+  font-size: 20px;
+  border-radius: 5px;
+}
+
+.btn:hover {
+  background-color: #cddcff;
+  color: rgb(1, 1, 48);
+  filter: drop-shadow(-10px 10px 10px #7c83a5);
 }
 </style>
